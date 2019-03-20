5770d12d51b9cdde0f57e88ac37c13e4c14df029
JC324332 <stephen.pritchard@my.jcu.edu.au>
2019-03-20 10:11:37 +1000

    Refactor program into seperate functions.

--- cp1404Practicals/prac_03/password_entry.py
+++ cp1404Practicals/prac_03/password_entry.py
@@ -1,6 +1,21 @@
-MIN_PASSWORD_LENGTH = 6
-password = input("Enter a password: ")
-while len(password) < MIN_PASSWORD_LENGTH:
-    print("Invalid password! Must be {} or more characters!".format(MIN_PASSWORD_LENGTH))
-    password = input("Enter a password:")
-print("*" * len(password))
+def main():
+
+    MIN_PASSWORD_LENGTH = 6
+    password = get_password(MIN_PASSWORD_LENGTH)
+    print(convert_password_to_stars(password))
+
+
+def get_password(min_password_length):
+    """Get password, check for length."""
+    password = input('Enter a password: ')
+    while len(password) < min_password_length:
+        print("Invalid password! Must be {} or more characters!".format(min_password_length))
+        password = input("Enter a password:")
+    return password
+
+
+def convert_password_to_stars(password):
+    return "*" * len(password)
+
+
+main()
